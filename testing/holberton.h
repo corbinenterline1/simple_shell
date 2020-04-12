fa0d3777caeaae64b042bcfb5d801a10d2e67017
Corbin Enterline <1692@holbertonschool.com>
2020-04-12 23:23:23 +0000

    Updated header

---
+++ testing/holberton.h
@@ -0,0 +1,31 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+void _printppid(void);
+int _putchar(char c);
+char *read_input(char *s);
+char **strtokarray(char *str);
+void freeptrarray(char **ptr);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+int execute_input(char *argv[]);
+char *_getenv(const char *name);
+extern char **environ;
+typedef struct pathlist
+{
+	char *str;
+	struct pathlist *next;
+} list_p;
+list_p *add_node(list_p **head, char *str);
+list_p *createpathlist(char *str);
+size_t print_list(const list_p *h);
+void free_list(list_p *head);
+list_p *pathlist(void);
+#endif /*HOLBERTON_H*/
